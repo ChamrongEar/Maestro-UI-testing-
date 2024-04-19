85e2954fda27b17d70aff996289ce89a07d2ab79
Ear Chamrong <earchamrong@Ears-MacBook-Pro-2.local>
2024-04-19 10:37:07 +0700

    add express working flow

--- android/driver-app/express/taxi-express.js
+++ android/driver-app/express/taxi-express.js
@@ -1,3 +1,6 @@
 output.taxiExpress = {
+    receivingItem: "Swipe right after receiving the item",
+    startDropoff: "Start Drop Off",
+    deliveryItem: "Swipe right after delivery the item",
     
 }
