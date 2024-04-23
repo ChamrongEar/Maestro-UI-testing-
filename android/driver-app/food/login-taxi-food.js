1e7a91286534bbd50dc404961c0b20a55e50e98e
Ear Chamrong <earchamrong@Ears-MacBook-Pro-2.local>
2024-04-23 16:26:01 +0700

    change phone number for taxi food

--- android/driver-app/food/login-taxi-food.js
+++ android/driver-app/food/login-taxi-food.js
@@ -1,6 +1,6 @@
 output.login = {
     phoneNumberfield: "លេខទូរស័ព្ទ / អុីមែល",
-    phoneNumber: "12332211",
+    phoneNumber: "123123127",
     passWordfield: "ពាក្យសម្ងាត់",
     password: "123456",
     loginBtn: "ចូលកម្មវិធី",
