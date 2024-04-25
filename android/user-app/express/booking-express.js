8ac73d9da268a17cbc178e2110e522e8970af6f5
Ear Chamrong <earchamrong@Ears-MacBook-Pro-2.local>
2024-04-25 14:30:54 +0700

    add checking express status on detail and edit store app

--- android/user-app/express/booking-express.js
+++ android/user-app/express/booking-express.js
@@ -7,12 +7,14 @@ output.bookingexpress= {
     recipient_pay: "Recipient Pay",
     confirmBtn: "Confirm",
     deliverTo: "Eden Garden, Boeung Kak Road, Boeung Kak Community, Khan Daun Penh, Phnom Penh, 120306, Cambodia",
-    inputcashcollection: "Enter Collection Amount ($)",
+    inputcashcollectionfield: "Enter Collection Amount ($)",
+    cashcollection: "25",
     proceedtopaybtn: "Proceed to Pay",
     nextBtn: "Next",
     cashondelivery: "Cash on Delivery",
     wallet: "Wallet Balance $67.29",
     requestorderBtn: "Request Order",
-    proceedtopaybtn2: "Proceed to Pay $ 0.77",
-    viewtrackingBtn: "View Tracking"
+    proceedtopaybtn2: "Proceed to Pay $ 0.78",
+    viewtrackingBtn: "View Tracking",
+    // orderHistory: "1 Eden Garden, Boeung Kak Road, Boeung Kak Community, Khan Daun Penh, Phnom Penh, 120306, Cambodia AATest • 011 111 111 25 Apr 2024 $0.78",
 }
