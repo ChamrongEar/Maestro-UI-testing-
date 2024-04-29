e8aa82da6a45cb4dcead389643cd25b02fc50da0
Ear Chamrong <earchamrong@Ears-MacBook-Pro-2.local>
2024-04-29 16:26:41 +0700

    Enhace slash noted for remember

--- android/express-cancel-from-admin/admin-cancel-after-driver-accepted.sh
+++ android/express-cancel-from-admin/admin-cancel-after-driver-accepted.sh
@@ -2,8 +2,9 @@ echo "Running maestro tests..."
 cd /Users/earchamrong/samples/android/user-app/express/
 maestro test express-flow.yaml
 
+# wait backend ...
 echo "Running Playwright tests..."
-cd /Users/earchamrong/v3-playwright/tests/V3/Scrap/Express/
+cd /Users/earchamrong/v3-playwright/tests/V3/Scrap/Express/                         
 npx playwright test --ui cancel.spec.ts
 
 echo "Running maestro tests..."
