141b43302103b357b6994e429febf793764d37d1
Ear Chamrong <earchamrong@Ears-MacBook-Pro-2.local>
2024-06-04 15:08:58 +0700

    Add e-ticket

---
+++ android/user-app/e-ticket/e-ticket.js
@@ -0,0 +1,9 @@
+output.eticket = {
+    service: "BOOKINGS",
+    store: "Sting Musicverse Festival Standard Ticket",
+    orderBtn: "ORDER NOW",
+    proceedtopayBtn: "PROCEED TO PAY",
+    wallet: "Wallet",
+    placeorderBtn: "PLACE THE ORDER NOW",
+    eticketTap: "E-TICKET",
+}
